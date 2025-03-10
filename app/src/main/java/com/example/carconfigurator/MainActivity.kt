38dceda80e6e87b446b3c98a3e5936ce5a30f22e
Szymonium <szymonkarpiszen@gmail.com>
2025-03-10 11:19:54 +0100

    Design Car App

--- app/src/main/java/com/example/carconfigurator/MainActivity.kt
+++ app/src/main/java/com/example/carconfigurator/MainActivity.kt
@@ -1,10 +1,16 @@
 package com.example.carconfigurator
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.provider.MediaStore.Audio.Radio
+import android.view.View
+import android.widget.ImageView
+import android.widget.RadioGroup
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.iterator
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +22,16 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val myRadio = findViewById<RadioGroup>(R.id.cars)
+        val myImg = findViewById<ImageView>(R.id.car_image)
+
+        for (radioButton in myRadio) {
+            if (radioButton.isActivated) {
+                myImg.setImageDrawable(Drawable.createFromPath("@drawable/${radioButton.text}"))
+            }
+        }
     }
+
+    fun send(view: View) {}
 }
